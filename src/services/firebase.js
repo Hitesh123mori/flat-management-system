590c58ac8b28b44caf51623401af09c4b9e59128
Hitest123mori <harshmori0521@gmail.com>
2025-07-07 15:30:02 +0530

    deleted

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,15 +5,6 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 // Replace with your actual Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCjAytJ2n0wlxrPcx5hrLQlP8Y1CN6B27k",
-  authDomain: "arms-b06fd.firebaseapp.com",
-  projectId: "arms-b06fd",
-  storageBucket: "arms-b06fd.firebasestorage.app",
-  messagingSenderId: "93721468926",
-  appId: "1:93721468926:web:32b299aa91ef85dd216aaf",
-  measurementId: "G-FV15CLT2EX"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
